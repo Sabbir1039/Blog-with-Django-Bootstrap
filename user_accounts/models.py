212d863064248bb0dddfbd6e103300b164e27c73
Sabbir Hossain <cse.170201039@gmail.com>
2023-11-09 23:00:34 +0600

    Profile model added to models.py, then urls.py and forms.py added to user_accounts app.

--- user_accounts/models.py
+++ user_accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_of_birth = models.DateField(null=True, blank=True, verbose_name = 'Date of Birth')
+    profile_pic = models.ImageField(default='default_pic.png', upload_to='profile_pics')
+    
+    def __str__(self) -> str:
+        return f'{self.user}'
+    
+    def get_absolute_url(self):
+        return reverse('profiles:profile-detail', args=[str(self.pk)])
