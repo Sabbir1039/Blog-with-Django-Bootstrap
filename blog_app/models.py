792c06ae9e772adbcaa32bf0f295bf4c0b88d565
Sabbir Hossain <cse.170201039@gmail.com>
2023-11-09 23:49:53 +0600

    models, views and urls file modified

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    categories = models.ManyToManyField(Category, related_name='posts')
+    is_published = models.BooleanField(default=True)
+    
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['created_at']
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post}"
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('post', 'user')  # Ensure each user can only like a post once.
+
+    def __str__(self):
+        return f"Like by {self.user} on {self.post}"
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,3 +1,19 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
-
+from .models import Post
+from django.views.generic.list import ListView
 # Create your views here.
+
+class HomePageView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    # context_object_name = 'posts'
+    
+    
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Home'
+        context['recent_post'] = Post.objects.all().order_by('created_at')[:5]
+        # context['top_post'] = Post.objects.all().count()
+        return context
