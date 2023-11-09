feb48757761d026ad369f8d4f72c23228d044874
Sabbir Hossain <cse.170201039@gmail.com>
2023-11-09 23:39:16 +0600

    blog_app app created

--- user_accounts/urls.py
+++ user_accounts/urls.py
@@ -12,20 +12,20 @@ urlpatterns = [
     path('update_profile/<int:pk>/', UserProfileUpdateView.as_view(), name = 'user_profile_update'),
     
     path('register/', user_accounts_views.UserRegistrationView.as_view(), name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name="user_accounts_app/login.html"), name = 'login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name="user_accounts_app/logout.html"), name = 'logout'),
+    path('login/', auth_views.LoginView.as_view(template_name="user_accounts/login.html"), name = 'login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name="user_accounts/logout.html"), name = 'logout'),
      # urls for password reset
     path('password-reset/', 
-         auth_views.PasswordResetView.as_view(template_name='user_accounts_app/password_reset.html'),
+         auth_views.PasswordResetView.as_view(template_name='user_accounts/password_reset.html'),
          name='password_reset'),
     path('password-reset/done/',
-         auth_views.PasswordResetDoneView.as_view(template_name='user_accounts_app/password_reset_done.html'),
+         auth_views.PasswordResetDoneView.as_view(template_name='user_accounts/password_reset_done.html'),
          name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/', 
-         auth_views.PasswordResetConfirmView.as_view(template_name='user_accounts_app/password_reset_confirm.html'),
+         auth_views.PasswordResetConfirmView.as_view(template_name='user_accounts/password_reset_confirm.html'),
          name='password_reset_confirm'),
     path('password-reset-complete/', 
-         auth_views.PasswordResetCompleteView.as_view(template_name='user_accounts_app/password_reset_complete.html'),
+         auth_views.PasswordResetCompleteView.as_view(template_name='user_accounts/password_reset_complete.html'),
          name='password_reset_complete'),
 ]
 if settings.DEBUG:
