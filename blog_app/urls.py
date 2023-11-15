64d3fd62e1a051641620a746dc958812d908ca90
Sabbir Hossain <cse.170201039@gmail.com>
2023-11-15 23:45:34 +0600

    post likes added to the home page

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('posts/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
-    path('post/new/', PostCreateView.as_view(), name='post-new'),
+    path('posts/new/', PostCreateView.as_view(), name='post-new'),
 ]
